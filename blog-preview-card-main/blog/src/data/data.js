a1cc04247efa6f19e6684ce95e71ad2e88ce5759
refraincr <xinchunzhong4@gmail.com>
2026-05-08 14:19:24 +0800

    对图片资源的访问做出优化

--- blog-preview-card-main/blog/src/data/data.js
+++ blog-preview-card-main/blog/src/data/data.js
@@ -1,13 +1,13 @@
-// import Img from "../public/illustration-article.svg"
-// import Avatar from "../public/image-avatar.webp"
+import Img from "@/assets/images/illustration-article.svg"
+import Avatar from "@/assets/images/image-avatar.webp" 
 
 export const info = [{
                     id:1,   
-                    img:"/images/illustration-article.svg",
+                    img:Img,
                     tags:["Learning"],
                     date:"Published 23 Dec 2023",
                     title:"HTML & CSS foundations",
                     content:"These languages are the backbone of every website, defining structure, content, and presentation.",
                     author:"Greg Hooper",
-                    avatar:"/images/image-avatar.webp"
+                    avatar:Avatar
                 }]
--- blog-preview-card-main/blog/vite.config.js
+++ blog-preview-card-main/blog/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: "/mentor_first/"
+  base: "/mentor_first/",
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,'./src')
+    }
+  }
 })
